26ad3812f459d55c10bbd9487500cba365107a2c
riyanks <riyankhoirisiregar1215@gmail.com>
2023-12-06 17:47:01 +0700

    new

--- src/app/status_sensor/sensor/status_acc/page.tsx
+++ src/app/status_sensor/sensor/status_acc/page.tsx
@@ -1,5 +1,5 @@
 import SensorDataStatus from '@/components/data/SensorDataStatus'
 
 export default function AccelerographPage() {
-  return <SensorDataStatus sensorType="Accelerograph" endpoint="http://localhost:3001/sensor/status_acc" />
+  return <SensorDataStatus sensorType="Accelerograph" endpoint="http://127.0.0.1:3001/sensor/status_acc" />
 }
